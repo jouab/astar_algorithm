ae8cb5c8ce3886a47c1a8b575837d2cdd7ae17c9
jouab <jblalbalat@gmail.com>
2019-12-22 18:10:48 +0100

    AA

--- extract data.c
+++ extract data.c
@@ -19,20 +19,39 @@ Queue whq;
 } AStarStatus;
 
 
-void ExitError(char miss, int errcode )
+void ExitError(const char *miss, int errcode )
 {
     fprintf ("stderr, \nERROR: %s.\nStopping...\n\n", miss);
     exit(errcode);
 }
 
-int main (int argc,char*argv[], int fin, int NULL)
-{
-    //FILE *fin;
-    unsigned long nnodes;
+int main (int argc,char*argv[]){
+    FILE *fin;
+    unsigned long nnodes=23895681UL
+    char name[257];
     node *nodes;
 
-    if((fin = fopen (argv[1], "r")) == NULL)
-        ExitError(1, 11);
+    if((nodes=(node *) malloc(nnodes*sizeof(node)) == NULL)
+        ExitError("when allocating memory for the nodes vector", 5);
 }
 
+/∗Computing the total number of successors∗/
+unsigned long ntotnsucc=0UL;
+for (i=0;i<nnodes,i++) ntotnsucc += nodes[i].nsucc
 
+strcpy(name,argv[1]); strcpy(strrchr(name,'.'), "bin")
+if ((fin=fopen(name,"wb"))==NULL)
+        ExitError("the output binary data file cannot be opened", 31);
+
+if  (fwrite(&nnodes, sizeof(unsigned long),1,fin)+fwrite(&ntotnsucc, sizeof(unsigned long),1,fin)!=2)
+        ExitError("when initializing the output binary data file", 32);
+
+if(fwrite(nodes, sizeof(node),nnodes,fin) != nnodes)
+        ExitError("when writing nodes to the output binary data file", 32);
+
+for (i=0; i<nnodes; i++) if(nodes[i].nsucc){
+        if(fwrite(nodes[i].successors,sizeof(unsigned long), nodes[i].nsucc,fin) != nodes[i].nsucc)
+                ExitError("when writing edges to the output binary data file", 32);
+}
+
+fclose(fin);
