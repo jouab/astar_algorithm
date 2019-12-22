1ccb248f8407e940aa3514e0c1dbb274e4f17795
jouab <jblalbalat@gmail.com>
2019-12-22 22:34:18 +0100

    llibreries

--- extract data.c
+++ extract data.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
 unsigned long id;          // Node identification
@@ -10,48 +13,52 @@ node;
 
 
 
-typedef char Queue;
+/*typedef char Queue;
 enum whichQueue {NONE, OPEN, CLOSED};
 
 typedef struct {double g, h;
 unsigned long parent;
 Queue whq;
-} AStarStatus;
+} AStarStatus;*/
 
 
-void ExitError(const char *miss, int errcode )
-{
+void ExitError(const char *miss, int errcode ){
     fprintf ("stderr, \nERROR: %s.\nStopping...\n\n", miss);
     exit(errcode);
 }
 
-int main (int argc,char*argv[]){
+int main (int argc,char *argv[]){
     FILE *fin;
-    unsigned long nnodes=23895681UL
+    unsigned long nnodes=23895681UL;
     char name[257];
     node *nodes;
+    if((nodes=(node *)malloc(nnodes*sizeof(node)) == NULL)) {
+        ExitError("when allocating memory for the nodes vector", 5);}
 
-    if((nodes=(node *) malloc(nnodes*sizeof(node)) == NULL)
-        ExitError("when allocating memory for the nodes vector", 5);
-}
 
-/∗Computing the total number of successors∗/
+/*Computing the total number of successors*/
 unsigned long ntotnsucc=0UL;
-for (i=0;i<nnodes,i++) ntotnsucc += nodes[i].nsucc
 
-strcpy(name,argv[1]); strcpy(strrchr(name,'.'), "bin")
-if ((fin=fopen(name,"wb"))==NULL)
-        ExitError("the output binary data file cannot be opened", 31);
-
-if  (fwrite(&nnodes, sizeof(unsigned long),1,fin)+fwrite(&ntotnsucc, sizeof(unsigned long),1,fin)!=2)
-        ExitError("when initializing the output binary data file", 32);
-
-if(fwrite(nodes, sizeof(node),nnodes,fin) != nnodes)
-        ExitError("when writing nodes to the output binary data file", 32);
-
-for (i=0; i<nnodes; i++) if(nodes[i].nsucc){
-        if(fwrite(nodes[i].successors,sizeof(unsigned long), nodes[i].nsucc,fin) != nodes[i].nsucc)
-                ExitError("when writing edges to the output binary data file", 32);
+for(int i=0; i<nnodes; i++){
+        ntotnsucc += nodes[i].nsucc;
 }
+strcpy(name,argv[1]);
+strcpy(strrchr(name,'.'), "bin");
+
+if((fin=fopen(name,"wb"))==NULL) {
+        ExitError("the output binary data file cannot be opened", 31);}
+
+if(fwrite(&nnodes, sizeof(unsigned long),1,fin)+fwrite(&ntotnsucc, sizeof(unsigned long),1,fin)!=2){
+        ExitError("when initializing the output binary data file", 32);}
+
+if(fwrite(nodes, sizeof(node),nnodes,fin) != nnodes){
+        ExitError("when writing nodes to the output binary data file", 32);}
+
+for (int i=0; i<nnodes; i++){
+    if(nodes[i].nsucc){
+        if(fwrite(nodes[i].successors,sizeof(unsigned long), nodes[i].nsucc,fin) != nodes[i].nsucc){
+                ExitError("when writing edges to the output binary data file", 32);}
+} }
 
 fclose(fin);
+}
