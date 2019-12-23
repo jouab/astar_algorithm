934c91a77d3625e5204dbdaeace22ea2565fcc78
natanaeljbort <45204945+natanaeljbort@users.noreply.github.com>
2019-12-23 19:14:48 +0100

    Astar

---
+++ astar_code.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct {
+unsigned long id;          // Node identification
+char *name;
+double lat, lon;           // Node position
+unsigned short nsucc;      // Number of node successors; i. e. length of successors
+unsigned long *successors;
+}
+node;
+
+node *Open, *Close;
+
+typedef char Queue;
+enum whichQueue {NONE, OPEN, CLOSED};
+
+typedef struct {double g, h;
+unsigned long parent;
+Queue whq;
+} AStarStatus;
+
+void main(int argc, char* argv[]){
+
+      Open    = (float*) malloc( (100)*sizeof(node) );
+      Close   = (float*) malloc( (100)*sizeof(node) );
+
+
+
+}
+
