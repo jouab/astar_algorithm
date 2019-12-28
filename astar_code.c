a286b2c6ea09c7635cf7ead2f5d8e7a9c083c923
Natanael <45204945+natanaeljbort@users.noreply.github.com>
2019-12-28 17:46:39 +0100

    Update astar_code.c

--- astar_code.c
+++ astar_code.c
@@ -14,22 +14,28 @@ else{return a;}
 /*Per al cas de catalunya*/
 //int Index_ultim=binarySearch(Vector.id, 8670491, 842755875, 429854583);
 
-float h(node_map nod, int i){
+float h(node nod[], unsigned long i){
 float R=6371000, a, c, d;
 float diflat, diflong;
-diflat= nod.lat - nod.lat;
-diflong= nod.lon - nod.lon;
+diflat= nod[i].lat - nod[i].lat;
+diflong= nod[i].lon - nod[i].lon;
 
-a= sin((diflat)/2)*sin((diflat)/2)+cos((nod.lat))*cos(nod.lat)*sin((diflong)/2)*sin((diflong)/2);
+a= sin((diflat)/2)*sin((diflat)/2)+cos((nod[i].lat))*cos(nod[i].lat)*sin((diflong)/2)*sin((diflong)/2);
 c= 2*asin(mini(1,sqrt(a)));
 d=R*c;
 
 return d;
 }
+float g(node node[],unsigned long i, unsigned long j){
+float g;
+g= sqrt(pow((node[i].lat-node[j].lat),2)+pow((node[i].lon-node[j].lon),2));
+return g;
+}
 
-float distancia(node *in){
-
-
+float f(node node[], unsigned long i, unsigned long j){
+float f;
+f=g(node,i,j)+h(node, i);
+return f;
 }
 
 void main(int argc, char* argv[]){
@@ -40,18 +46,34 @@ InicialitzaLlista (&OPENlist);
       Open    = (node*) malloc( (100)*sizeof(node) );
       Close   = (node*) malloc( (100)*sizeof(node) );
 
+unsigned long Index_primer, Index_ultim;
+Index_primer=binarySearch(Vector,8670491,842755875,771979683);
+Index_ultim=binarySearch(Vector,8670491,842755875,429854583);
 InsereixNodeInicial(&OPENlist);
 node_map *primer_node;
 primer_node =InicideLlista(OPENlist);
-(primer_node->f)=1/*funcio que calcula f per al node inicial*/ ,
-(primer_node->id)=771979683,
-(primer_node->lat)=1/*punter a la latitud del node inicial*/,
-(primer_node->lon)=1/*punter a la longitud edl node inicial*/,
-(primer_node->name)=1/*punter que va al nom del node inicial*/;
-
+(primer_node->f)=h(Vector,Index_primer)/*funcio que calcula f per al node inicial*/ ,
+(primer_node->id)=771979683;
+//(primer_node->lat)=1/*punter a la latitud del node inicial*/,
+//(primer_node->lon)=1/*punter a la longitud edl node inicial*/,
+//(primer_node->name)=1/*punter que va al nom del node inicial*/;
 
 while(EsPlena(OPENlist)){
-printf("bucle");
+node_map *node_cu;
+node_cu = InicideLlista(OPENlist);
+unsigned long index_curr;
+index_curr = binarySearch(Vector,8670491,842755875,(node_cu->id));
+if((node_cu->id) == 429854583){
+    printf(("s'ha arribat al desti!!"));
+    break;
+}
+//for(i=0;i<Vector.nsucc;i++){
+//node_map node_suc;
+//node
+//
+//}
+
+
 }
 
 
