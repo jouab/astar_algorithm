a5e1f114985ed518d5b7a1ff88e6a7c8da273d4a
Natanael <45204945+natanaeljbort@users.noreply.github.com>
2019-12-30 12:20:47 +0100

    Update astar_code.c

--- astar_code.c
+++ astar_code.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "lib_Astar.h"
 
-node *Vector, *Open, *Close;
+node *Vector;
 
 float mini(float a,float b){
 if(a>b){return b;}
@@ -26,16 +26,10 @@ d=R*c;
 
 return d;
 }
-float g(node node[],unsigned long i, unsigned long j){
-float g;
-g= sqrt(pow((node[i].lat-node[j].lat),2)+pow((node[i].lon-node[j].lon),2));
-return g;
-}
-
-float f(node node[], unsigned long i, unsigned long j){
-float f;
-f=g(node,i,j)+h(node, i);
-return f;
+float w(node node[],unsigned long i, unsigned long j){
+float w;
+w= sqrt(pow((node[i].lat-node[j].lat),2)+pow((node[i].lon-node[j].lon),2));
+return w;
 }
 
 void main(int argc, char* argv[]){
@@ -44,16 +38,15 @@ map OPENlist, CLOSEDlist;
 InicialitzaLlista (&OPENlist);
 InicialitzaLlista (&CLOSEDlist);
 
-      Open    = (node*) malloc( (100)*sizeof(node) );
-      Close   = (node*) malloc( (100)*sizeof(node) );
-
 unsigned long Index_primer, Index_ultim;
 Index_primer=binarySearch(Vector,8670491,842755875,771979683);
 Index_ultim=binarySearch(Vector,8670491,842755875,429854583);
 InsereixNodeInicial(&OPENlist);
 node_map *primer_node;
 primer_node =InicideLlista(OPENlist);
-(primer_node->f)=h(Vector,Index_primer)/*funcio que calcula f per al node inicial*/ ,
+(primer_node->f)=h(Vector,Index_primer);/*funcio que calcula f per al node inicial*/
+(primer_node->h)=h(Vector,Index_primer);
+(primer_node->g)=0;
 (primer_node->id)=771979683;
 (primer_node->parentid)=NULL;
 
@@ -72,36 +65,54 @@ node_map nod;
 //for (nod=OPENlist.start; nod<OPENlist.end; nod=nod->seg){
 
 unsigned long index_succ=binarySearch(Vector,8670491,842755875,(node_suc->id));
-double successor_current_cost=h(Vector,index_succ)+g(Vector,index_curr,index_succ);
+double successor_current_cost=(node_cu->g)+w(Vector,index_curr,index_succ);
     if(CercaSequencial_id(node_suc->id, OPENlist)!=NULL){
-        if(g(Vector,index_curr,index_succ)<=successor_current_cost){
+        if((node_suc->g)<=successor_current_cost){
             continue;
              }
         }
     else if(CercaSequencial_id(node_suc->id, CLOSEDlist)!=NULL){
-        if(g(Vector,index_curr,index_succ)<=successor_current_cost){
+        if((node_suc->g)<=successor_current_cost){
+            InsereixNodeFinal(&OPENlist);
+            node_map *node_suop = FinaldeLlista(OPENlist);
+            (node_suop->f)=(node_suc->f)/*funcio que calcula f per al node inicial*/ ,
+            (node_suop->id)=(node_suc->id);
+            (node_suop->parentid)=(node_suc->parentid);
+            (node_suop->g)=(node_suc->g);
+            (node_suop->h)=(node_suc->h);
+            BorraNode(node_suc, &CLOSEDlist);
+
             continue;
+
+            //Move node_successor from the CLOSED list to the OPEN list
              }
         }
     else{//Add node to the open list
             InsereixNodeFinal(&OPENlist);
             node_suc =FinaldeLlista(OPENlist);
-            (node_suc->f)=successor_current_cost/*funcio que calcula f per al node inicial*/ ,
             (node_suc->id)=index_succ;
-            (node_suc->parentid)=(node_cu->id);
+            (node_suc->h)=h(Vector,index_succ);
         }
+    (node_suc->parentid)=(node_cu->id);
+    (node_suc->g)=successor_current_cost;
+    (node_suc->f)=(node_suc->g)+(node_suc->h);
+
     //}
     //Add current node to the closed list,
     if(EsBuida(CLOSEDlist)){
         InsereixNodeInicial(&CLOSEDlist);
         node_map *node_cuclos = InicideLlista(CLOSEDlist);
-        (node_cuclos->f)=(node_cu->f)/*funcio que calcula f per al node inicial*/ ,
+        (node_cuclos->h)=(node_cu->h);
+        (node_cuclos->g)=(node_cu->g);
+        (node_cuclos->f)=(node_cu->f)/*funcio que calcula f per al node inicial*/;
         (node_cuclos->id)=(node_cu->id);
         (node_cuclos->parentid)=(node_cu->parentid);
         }
     else{InsereixNodeFinal(&CLOSEDlist);
         node_map *node_cuclos = FinaldeLlista(CLOSEDlist);
-        (node_cuclos->f)=(node_cu->f)/*funcio que calcula f per al node inicial*/ ,
+        (node_cuclos->h)=(node_cu->h);
+        (node_cuclos->g)=(node_cu->g);
+        (node_cuclos->f)=(node_cu->f)/*funcio que calcula f per al node inicial*/;
         (node_cuclos->id)=(node_cu->id);
         (node_cuclos->parentid)=(node_cu->parentid);
     }
@@ -116,4 +127,3 @@ if((node_suc->id) != 429854583){
     printf("\nError, La llista OPEN esta buida");
 }
 }
-
